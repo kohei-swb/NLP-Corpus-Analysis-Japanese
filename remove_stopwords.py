f8786ab7da782f9698ebf22b259239e4f5fc1c4c
Kohei <1lovehamuhamu.15@gmail.com>
2024-07-08 19:30:29 +0900

    Add manual stop words

--- remove_stopwords.py
+++ remove_stopwords.py
@@ -12,6 +12,13 @@ def get_stopwords_list():
         if len(tmp[i]) < 1:
             continue
         stopwords.append(tmp[i])
+        
+    # Manually adding stop words
+    additional_stopWords = ["0", "00", "000", "0パーセント", "0件", "0度", "0ユーロ", "0km", "0分", "0倍", "0ドル", "0メートル", "0mm", "0点", "00000", "の", "ら", "ころ", "および", "ap", "hp", "ド", "か月", "にも", "した", "では", "(", ")", "同国", "0回", "彼ら", "との", "しない", "ドニ", "hp", "ap", "eu", "世紀", "年", "月", "日", "時", "分", "秒", "メートル", "キロメートル", "センチメートル", "グラム", "キログラム", "リットル", "これ", "それ", "あれ", "ここ", "そこ", "あそこ", "どこ", "だれ", "何", "する", "いる", "ある", "なる", "こと", "もの"]
+    
+    for i in range(len(additional_stopWords)):
+        stopwords.append(additional_stopWords[i])
+
     return stopwords
 
 def remove_from_list(list_stoppingWords, text):
